f583b0e2e80a34a359cc6bcb05a4fe9d40dcdca9
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2016-08-23 12:31:30 +0200

    Revert "Basic catkin tree"
    This reverts commit b64be2b734ead92f002d0a439cfe818caef31d80.

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wasp_vb/wasp-summer-school-team6/catkin_ws/src
-# Build directory: /home/wasp_vb/wasp-summer-school-team6/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)

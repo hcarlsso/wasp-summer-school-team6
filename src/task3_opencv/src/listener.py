a752ebfcda6610513d80c1c114bee61b236002d6
Alexandre Martins (a.k.a Maral Afris) <maral.afris@gmail.com>
2016-08-26 09:41:40 +0200

    add listener

---
+++ src/task3_opencv/src/listener.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from wasp_custom_msgs.msg import object_loc
+
+ids = {};
+
+def callback(data):
+    global ids
+
+    if str(data.ID) not in ids:
+        rospy.loginfo("Callback had: %s", ids.keys())
+        rospy.loginfo("Callback adds: %s", str(data.ID))
+        ids[str(data.ID)] = data
+
+def listener():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("/object_location", object_loc, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
